ad189e36d8ebbaffdecfd9127f23b54f4ea397bd
BraedenHCL <braeden.norman6@gmail.com>
2023-03-30 21:47:24 -0700

    started implementing build check endpoint

--- app/api/endpoints/build.py
+++ app/api/endpoints/build.py
@@ -6,8 +6,8 @@ import requests
 router = APIRouter(prefix=base_path, tags=["example"])
 
 
-@router.get("/build")
-async def example(TO_BUILD_REPO: str):
+@router.post("/build")
+async def start_build(TO_BUILD_REPO: str):
     # return {"message": hello_world}
     # Set up variables for the GitHub repository and workflow
     REPO_OWNER = "The-Programming-Lab"
@@ -39,11 +39,35 @@ async def example(TO_BUILD_REPO: str):
 
     # Send the request to trigger the workflow
     response = requests.post(API_ENDPOINT, headers=headers, json=data)
+    run_id = response.headers['X-GitHub-Request-Id']
+    # print(response.workflow_id)
 
     # Check the response status code
     if response.status_code == 204:
-        return {"message": "Workflow dispatched successfully."}
+        return {"message": "Workflow dispatched successfully.", "run_id": run_id }
     else:
         return {"message": f"Failed to dispatch workflow. Response code: {response.status_code}"}
 
+
+@router.get("/build/done")
+async def check_build_done(workflow_id: str):
+    # Set the GitHub repository and build ID
+    repo = 'my-org/my-repo'
+    build_id = '123456'
+
+    # Set the GitHub access token (replace with your own token)
+    access_token = GITHUB_TOKEN
+
+    # Define the GitHub API endpoint for checking the build status
+    url = f'https://api.github.com/repos/{repo}/actions/runs/{build_id}'
+
+    headers = {'Authorization': f'token {access_token}'}
+
+    # Send a GET request to the API endpoint to check the build status
+    response = requests.get(url, headers=headers)
+
+    data = response.json()
+
+
+    return { "message": "Workflow found.", "status": data['status'], "workflow_id": workflow_id }
 
